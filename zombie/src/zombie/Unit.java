3ae3682a55c0b91666837d8b63682fb62686b024
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 13:58:39 +0900

    Modify: Unit Class(getter & setter add)

--- zombie/src/zombie/Unit.java
+++ zombie/src/zombie/Unit.java
@@ -1,5 +1,34 @@
 package zombie;
 
+import java.util.Random;
+
 public class Unit {
+	private Random ran;
+	private int hp;			// 체력
+	private int position;	// 위치
+	private int maxHp;		// 최대 체력
 	
+	public Unit(int position, int hp, int maxHp) {
+		this.position = position;
+		this.hp = hp;
+		this.maxHp = maxHp;
+		ran = new Random();
+	}
+	
+	public int getPosition() {
+		return this.position;
+	}
+	public int getHp() {
+		return this.hp;
+	}
+	public int getMaxHp() {
+		return this.maxHp;
+	}
+	
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
 }
