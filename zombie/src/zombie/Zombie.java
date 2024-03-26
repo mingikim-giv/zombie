ea5029b562fe36c1d173ee27cd6cb12f275a42dd
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 15:18:58 +0900

    Modify: Zombie Class(attack Method)

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -10,6 +10,14 @@ public class Zombie extends Unit {
 	@Override
 	public void attack(Unit hero) {
 		power = ran.nextInt(maxHp);
+		
+		hero.setHp(hero.getHp() - power);
+		if(hero.getHp() <= 0) {
+			hero.setHp(0);
+		}
+		
+		System.out.printf("좀비가 %d의 파워로 공격\n", power);
+		System.out.printf("영웅[HP: %d]\n", hero.getHp());
 	}
 	
 }
