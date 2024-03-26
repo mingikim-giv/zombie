40f86ca95c6ac2aa6f325ea35d8ed4108b39fe58
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 14:08:58 +0900

    Modify: Unit Class(move, attack Method add)

--- zombie/src/zombie/Unit.java
+++ zombie/src/zombie/Unit.java
@@ -2,11 +2,13 @@ package zombie;
 
 import java.util.Random;
 
-public class Unit {
-	private Random ran;
-	private int hp;			// 체력
-	private int position;	// 위치
-	private int maxHp;		// 최대 체력
+abstract public class Unit {
+	Random ran;
+	private final int SIZE = 20;// 맵 사이즈
+	
+	private int hp;				// 체력
+	private int position;		// 위치
+	private int maxHp;			// 최대 체력
 	
 	public Unit(int position, int hp, int maxHp) {
 		this.position = position;
@@ -31,4 +33,11 @@ public class Unit {
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
+	
+	public void move() {
+		if(position <= SIZE) {
+			System.out.printf("[현재 위치]: %d\n", position);
+		}
+	}
+	abstract void attack(Unit unit);
 }
