1e29bd7db7b6486df9d396d069b13a4e219db534
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 15:09:16 +0900

    Modify: Zombie Class

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -1,5 +1,15 @@
 package zombie;
 
-public class Zombie {
+public class Zombie extends Unit {
+	private int power;	// 파워
+	
+	public Zombie(int position, int hp, int maxHp) {
+		super(position, hp, maxHp);
+	}
 
+	@Override
+	public void attack(Unit hero) {
+		power = ran.nextInt(maxHp);
+	}
+	
 }
