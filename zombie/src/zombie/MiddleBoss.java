3c5b657a8cb3ec851a7cf34c5dc81437914049f8
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 18:38:20 +0900

    Modify: MiddleBoss Class add

---
+++ zombie/src/zombie/MiddleBoss.java
@@ -0,0 +1,22 @@
+package zombie;
+
+public class MiddleBoss extends Unit {
+	private int power;	// 파워
+	
+	public MiddleBoss(int position, int hp, int maxPower) {
+		super(position, hp, maxPower);
+	}
+
+	@Override
+	public void attack(Unit hero) {
+		power = ran.nextInt(maxPower);
+		
+		hero.setHp(hero.getHp() - power);
+		if(hero.getHp() <= 0) {
+			hero.setHp(0);
+		}
+		
+		System.out.printf("중간 보스가 %d의 파워로 공격\n", power);
+		System.out.printf("HERO[HP: %d]\n", hero.getHp());
+	}
+}
