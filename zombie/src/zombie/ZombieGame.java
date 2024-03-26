760afb69fffe9ca1dacc2fb5e7b8cecaa67675b3
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 15:08:50 +0900

    Modify: ZombieGame Class

--- zombie/src/zombie/ZombieGame.java
+++ zombie/src/zombie/ZombieGame.java
@@ -2,12 +2,12 @@ package zombie;
 
 public class ZombieGame {
 	
-	public static void run() {
-		
-	}
-	
 	public static void main(String[] args) {
 		ZombieGame.run();
+	}
+	
+	public static void run() {
+		Game.getInstance().run();
 	}
 
 }
