b59314117c9a83e2a2abb6ec6e4f299269346101
mingikim-giv <mingikim.giv@gmail.com>
2024-03-26 14:36:15 +0900

    Modify: Boss Class(getter & setter add)

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,5 +1,24 @@
 package zombie;
 
-public class Boss {
-
+public class Boss extends Unit {
+	private int protective;	// 보호막
+	
+	public Boss(int position, int hp, int maxHp) {
+		super(position, hp, maxHp);
+	}
+	
+	public int getProtective() {
+		return this.protective;
+	}
+	
+	public void setProtective(int protective) {
+		this.protective = protective;
+	}
+	
+	@Override
+	public void attack(Unit hero) {
+		
+	}
+	
+	
 }
