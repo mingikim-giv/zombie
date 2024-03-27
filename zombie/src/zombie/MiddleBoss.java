044366a853803a9d510a72f82f1148a8c08a5bb9
mingikim-giv <mingikim.giv@gmail.com>
2024-03-27 12:13:34 +0900

    Modify: MiddleBoss Class(attack Method)

--- zombie/src/zombie/MiddleBoss.java
+++ zombie/src/zombie/MiddleBoss.java
@@ -19,14 +19,33 @@ public class MiddleBoss extends Zombie {
 	
 	@Override
 	public void attack(Unit hero) {
-		power = ran.nextInt(maxPower);
+		int skillRan = ran.nextInt(3);
 		
-		hero.setHp(hero.getHp() - power);
-		if(hero.getHp() <= 0) {
-			hero.setHp(0);
+		if(skillRan == 1) {
+			System.out.println("중간 보스 스킬 발동!");
+			power = skill;
+			int damage = hero.getHp() - power;
+			hero.setHp(damage);
+			
+			if(hero.getHp() <= 0) {
+				hero.setHp(0);
+			}
+			
+			System.out.printf("중간 보스가 %d의 파워로 공격!\n", power);
+			System.out.printf("HERO[HP: %d]\n", hero.getHp());
 		}
-		
-		System.out.printf("중간 보스가 %d의 파워로 공격\n", power);
-		System.out.printf("HERO[HP: %d]\n", hero.getHp());
+		else {
+			System.out.println("중간 보스 공격!");
+			power = ran.nextInt(maxPower);
+			int damage = hero.getHp() - power;
+			hero.setHp(damage);
+			
+			if(hero.getHp() <= 0) {
+				hero.setHp(0);
+			}
+			
+			System.out.printf("중간 보스가 %d의 파워로 공격\n", power);
+			System.out.printf("HERO[HP: %d]\n", hero.getHp());
+		}
 	}
 }
